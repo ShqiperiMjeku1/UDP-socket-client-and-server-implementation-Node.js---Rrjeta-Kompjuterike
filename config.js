fee72953c8e1c294f682a41dde90781fc8d6e19f
Lis Spahija <lispahija@gmail.com>
2025-11-15 23:53:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,8 @@ module.exports = {
 
   // IP e serverit në rrjet lokal (LAN)
   SERVER_IP: "10.180.78.144",
+  MAX_CLIENTS: 4,
+  CLIENT_TIMEOUT_MS: 60_000,
+  SHARED_FOLDER: path.join(__dirname, "shared"),
+  STATS_LOG_FILE: path.join(__dirname, "server_stats.txt"),
+};
