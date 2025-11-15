dd4f8a1a1fe38a39d945f5e5a2f3e9a12241190b
enisshabani <shabanienis44@gmail.com>
2025-11-16 00:12:13 +0100

    Updated client.js

--- client.js
+++ client.js
@@ -30,3 +30,55 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+function sendToServer(commandLine) {
+  const payload = `${clientId}|${role}|${commandLine}`;
+  const buffer = Buffer.from(payload, "utf8");
+  clientSocket.send(buffer, SERVER_PORT, SERVER_IP, (err) => {
+    if (err) console.error("Error në dërgim:", err);
+  });
+}
+
+// Trajtimi i inputit nga user-i
+function handleUserInput(line) {
+  line = line.trim();
+  if (!line) return;
+
+
+  if (line.startsWith("/list")) {
+    sendToServer("LIST");
+  } else if (line.startsWith("/read ")) {
+    const filename = line.slice(6).trim();
+    sendToServer(`READ ${filename}`);
+  } else if (line.startsWith("/upload ")) {
+    const localPath = line.slice(8).trim();
+    fs.readFile(localPath, (err, data) => {
+      if (err) {
+        console.error("Nuk mund të lexoj file lokal:", err.message);
+        return;
+      }
+      const base64 = data.toString("base64");
+      const filename = path.basename(localPath);
+      sendToServer(`UPLOAD ${filename} ${base64}`);
+      console.log(`(Duke dërguar ${filename} në server)`);
+    });
+  } else if (line.startsWith("/download ")) {
+    const filename = line.slice(10).trim();
+    sendToServer(`DOWNLOAD ${filename}`);
+  } else if (line.startsWith("/delete ")) {
+    const filename = line.slice(8).trim();
+    sendToServer(`DELETE ${filename}`);
+  } else if (line.startsWith("/search ")) {
+    const keyword = line.slice(8).trim();
+    sendToServer(`SEARCH ${keyword}`);
+  } else if (line.startsWith("/info ")) {
+    const filename = line.slice(6).trim();
+    sendToServer(`INFO ${filename}`);
+  } else {
+    // mesazh i thjeshtë tekstual
+    sendToServer(`MSG ${line}`);
+  }
+}
+
+
+rl.on("line", handleUserInput);
